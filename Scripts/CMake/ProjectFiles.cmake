ea5b68f6ba74bfa766db51ac3875376f9a954dfd
KrisFis <krisfis.job@post.cz>
2021-10-15 10:35:28 +0200

    Removed redundant file and fixed include paths for ProjectFiles.cmake

--- Scripts/CMake/ProjectFiles.cmake
+++ Scripts/CMake/ProjectFiles.cmake
@@ -1,29 +1,30 @@
 
-function(ASTD_include_directories CURRENT_DIR)
+function(ASTD_file_public_directories CURRENT_DIR DIRECTORIES)
 
-include_directories(
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Macros"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Macros/Core"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Macros/Ensure"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Primitives"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Primitives/Misc"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Shared"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Shared/Misc"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/Shared/Types"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/TypeTraits"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/TypeTraits/Core"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Public/TypeTraits/Misc")
+set(${DIRECTORIES}
+    "${CURRENT_DIR}/../../ASTD/Source/Public"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Macros"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Macros/Core"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Macros/Ensure"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Primitives"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Primitives/Misc"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Shared"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Shared/Misc"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/Shared/Types"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/TypeTraits"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/TypeTraits/Core"
+    "${CURRENT_DIR}/../../ASTD/Source/Public/TypeTraits/Misc"
+    PARENT_SCOPE)
 
 endfunction()
 
 
-function(ASTD_file_sources CURRENT_DIR SOURCES)
+function(ASTD_file_private_files CURRENT_DIR FILES)
 
-file(GLOB_RECURSE "ASTD_SOURCES"
-    "${CURRENT_DIR}/../../${PROJECT_NAME}/Source/Private/*.cpp" 
+file(GLOB_RECURSE "PRIVATE_FILES"
+    "${CURRENT_DIR}/../../ASTD/Source/Private/*.cpp" 
 )
 
-set(${SOURCES} ${ASTD_SOURCES} PARENT_SCOPE)
+set(${FILES} ${PRIVATE_FILES} PARENT_SCOPE)
 
 endfunction()
