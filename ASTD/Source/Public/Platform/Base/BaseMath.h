3e5740dffdf520942353b47d0a7117ecf9309300
Jan Kristian Fisera <krisfis@email.cz>
2022-02-09 02:42:19 +0100

    Added Floor and Ceil

--- ASTD/Source/Public/Platform/Base/BaseMath.h
+++ ASTD/Source/Public/Platform/Base/BaseMath.h
@@ -3,6 +3,9 @@
 
 #include "Platform/PlatformTypes.h"
 
+// TODO(jan.kristian.fisera): Add custom implementations!
+#include <math.h>
+
 struct SBasePlatformMath
 {
 	template<typename T>
@@ -16,6 +19,12 @@ struct SBasePlatformMath
 
 	template<typename T>
 	FORCEINLINE static T Abs(T Value) { return Value >= (T)0 ? Value : -Value; }
+
+	template<typename T>
+	FORCEINLINE static T Floor(T Value) { return floor(Value); }
+
+	template<typename T>
+	FORCEINLINE static T Ceil(T Value) { return ceil(Value); }
 
 	template <typename T>
 	FORCEINLINE static bool IsPowerOfTwo(T Value) { return ((Value & (Value - 1)) == (T)0); }
