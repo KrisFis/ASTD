0fa3da806c2652d594204bb6b636539361b59d82
Jan Kristián Fišera <ikrisfis@icloud.com>
2025-01-09 18:42:04 -0500

    Split necessary ASTD.h includes to ASTDMinimal.h

--- include/ASTD/ASTD.h
+++ include/ASTD/ASTD.h
@@ -6,16 +6,10 @@
 // * Should include all features and functionality of ASTD
 /////////////////////////////////////////
 
-// Look at "Configurations.h" to see defines to that affects features provided
+// Look at "Configurations.h" to see defines that affects features provided
 
-// IMPORTANT CORE
-#include "ASTD/Core/Build.h"
-#include "ASTD/Core/Configuration.h"
-
-// TYPE MANIPULATION/GENERATION/SFINAE
-#include "ASTD/TypeMethods.h"
-#include "ASTD/TypeTraits.h"
-#include "ASTD/TypeTraitsMacros.h"
+// CORE
+#include "ASTD/ASTDMinimal.h"
 
 // UTILITIES
 #include "ASTD/Math.h"
@@ -40,10 +34,6 @@
 #include "ASTD/SharedClass.h"
 #include "ASTD/SharedObject.h"
 #include "ASTD/SharedUtils.h"
-
-// EXTRAS -> CHECKS
-#include "ASTD/Check.h"
-#include "ASTD/IsValid.h"
 
 // EXTRAS -> ARCHIVE
 #include "ASTD/Archive.h"
---
+++ include/ASTD/ASTDMinimal.h
@@ -0,0 +1,22 @@
+// Copyright Alternity Arts. All Rights Reserved
+
+#pragma once
+
+// Minimal PCH
+// * Should include minimal features
+/////////////////////////////////////////
+
+// Look at "Configurations.h" to see defines that affects features provided
+
+// IMPORTANT CORE
+#include "ASTD/Core/Build.h"
+#include "ASTD/Core/Configuration.h"
+
+// TYPE MANIPULATION/GENERATION/SFINAE
+#include "ASTD/TypeMethods.h"
+#include "ASTD/TypeTraits.h"
+#include "ASTD/TypeTraitsMacros.h"
+
+// CHECKS
+#include "ASTD/Check.h"
+#include "ASTD/IsValid.h"
